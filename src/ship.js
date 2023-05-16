33c29e677d559854cca25d9e15323569ee264801
ColdBlood237 <115800548+ColdBlood237@users.noreply.github.com>
2023-05-16 17:58:40 +0200

    Write the Ship factory function and make it pass all its tests

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,15 @@
-function Ship() {}
+function Ship(length) {
+  let hits = 0;
+
+  function hit() {
+    this.hits++;
+  }
+
+  function isSunk() {
+    return this.hits == this.length ? true : false;
+  }
+
+  return { length, hits, hit, isSunk };
+}
 
 export default Ship;
